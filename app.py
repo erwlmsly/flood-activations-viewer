97077f24ed61310680a97fd718281f651d87fbc5
erwlmsly <135661844+erwlmsly@users.noreply.github.com>
2025-06-11 18:21:38 +0000

    fixed bug with annual total, render map after selection

--- app.py
+++ app.py
@@ -8,23 +8,24 @@ from utils.google_sheets import fetch_google_sheet_data
 
 st.title("Expected Flood Activations")
 
-
-# Cache the data fetching function
-@st.cache_data
-def get_cached_google_sheet_data(sheet_url):
-    return fetch_google_sheet_data(sheet_url)
-
-
 # Fetch data from Google Sheets
 sheet_url = "https://docs.google.com/spreadsheets/d/1n7-vsmqASOU6lTc9GVB6Gl4yWA19BC7f60CqT3VI-8k"
-data = get_cached_google_sheet_data(sheet_url)
+
+st.subheader(
+    f"Visualize the expected ICEYE flood activations for the selected month(s) based on data from [Pyry's forecast]({sheet_url})."
+)
+
+data = fetch_google_sheet_data(sheet_url)
 
 # Dropdown to select the month
-month: List[str] | str = st.multiselect("Select a month or months:", data.columns[2:])
+month: List[str] | str = st.multiselect(
+    "Select a month(s) to render a map below:", data.columns[2:]
+)
 print(month)
 
 # Generate and display the map
 map = generate_map(data, month)
-map_html = map._repr_html_()
+if map:
+    map_html = map._repr_html_()
 
-html(map_html, height=1200, width=1200)  # Embed the map in Streamlit
+    html(map_html, height=1200, width=1200)  # Embed the map in Streamlit
